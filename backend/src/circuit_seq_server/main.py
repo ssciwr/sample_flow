1816d1c0f0fb5b589bae5c8a3253a8d594113355
Liam Keegan <liam@keegan.ch>
2022-11-09 20:00:26 +0100

    add data_path option to circuit_seq_server with . as default value for dev server

--- backend/src/circuit_seq_server/main.py
+++ backend/src/circuit_seq_server/main.py
@@ -6,8 +6,9 @@ from circuit_seq_server import create_app
 @click.command()
 @click.option("--host", default="127.0.0.1", show_default=True)
 @click.option("--port", default=5000, show_default=True)
-def main(host: str, port: int):
-    app = create_app()
+@click.option("--data-path", default=".", show_default=True)
+def main(host: str, port: int, data_path: str):
+    app = create_app(data_path=data_path)
     app.run(host=host, port=port)
 
 
